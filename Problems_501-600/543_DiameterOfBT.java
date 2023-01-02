c5d76c517b072244c23e42cfefeb3761e69671a2
Subramanya K S <subramanyaks2205@gmail.com>
2023-01-02 07:33:14 +0530

    updated 543

---
+++ Problems_501-600/543_DiameterOfBT.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int d[]= new int[1];
+        height(root,d);
+        return d[0];
+    }
+    public int height(TreeNode node, int d[]){
+        if(node==null){
+            return 0;
+        }
+        int l=height(node.left,d);
+        int r=height(node.right,d);
+        d[0]=Math.max(d[0],l+r);
+        return 1+Math.max(l,r);
+    }
+}
