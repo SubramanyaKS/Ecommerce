4abc3054ea6d8ddbdd784fc3805ce608e8dd91b8
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-16 22:43:20 +0530

    updated longest substring without repeat charecter

---
+++ Problems_1-100/3_LongestSubString.java
@@ -0,0 +1,25 @@
+class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int longestLength=0;
+            Map <Character,Integer> map = new LinkedHashMap<Character,Integer>();
+            char arr [] = s.toCharArray();
+            for(int i=0;i<arr.length;i++){
+                char c = arr[i];
+                 if(!map.containsKey(c)){
+                map.put(c,i);
+            }
+            else{
+                i=map.get(c);
+                map.clear();
+            }
+            if(map.size()> longestLength){
+                longestLength=map.size();
+            }
+            }
+           
+            return longestLength;
+    
+    
+            
+        }
+}
